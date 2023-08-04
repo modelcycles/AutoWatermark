bb3582f62bf08b22a6b2eaafe8b3be1b737d1b67
ohs980111 <ohs980111@postech.ac.kr>
2023-08-04 16:25:12 +0900

    added watermark size and location

--- AutoWatermark.py
+++ AutoWatermark.py
@@ -22,7 +22,6 @@ class MainWindow(QWidget, Ui_MainWindow):
         self.show()
 
     def initUI(self):
-        self.ratio = 0.08
         self.image_dir = ""
         self.images = []
         self.white = None
@@ -32,9 +31,32 @@ class MainWindow(QWidget, Ui_MainWindow):
 
         mainLayout = QVBoxLayout()
 
+        topLayout = QHBoxLayout()
         self.text = QTextBrowser(self)
         self.text.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        mainLayout.addWidget(self.text)
+        topLayout.addWidget(self.text)
+
+        self.groupBox = QGroupBox("Watermark Position")
+        self.groupBox.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        gridLayout = QGridLayout()
+
+        self.radioButtons = []
+        for row in range(3):
+            rowButtons = []
+            for col in range(3):
+                radioButton = QRadioButton()
+                gridLayout.addWidget(radioButton, row, col)
+                rowButtons.append(radioButton)
+                if row == 2 and col == 2:
+                    radioButton.setChecked(True)
+            self.radioButtons.append(rowButtons)
+
+        self.groupBox.setLayout(gridLayout)
+        topLayout.addWidget(self.groupBox)
+
+        topWidget = QWidget()
+        topWidget.setLayout(topLayout)
+        mainLayout.addWidget(topWidget)
 
         buttonLayout = QHBoxLayout()
 
@@ -50,12 +72,29 @@ class MainWindow(QWidget, Ui_MainWindow):
         self.button2.clicked.connect(self.button2Clicked)
         buttonLayout.addWidget(self.button2)
 
+        self.watermarkSizeInput = QDoubleSpinBox(self)
+        self.watermarkSizeInput.setRange(0, 1)
+        self.watermarkSizeInput.setSingleStep(0.01)
+        self.watermarkSizeInput.setValue(0.08)
+        self.watermarkSizeInput.setToolTip("Watermark size ratio (0 to 1)")
+        buttonLayout.addWidget(self.watermarkSizeInput)
+
         self.button3 = QPushButton("Apply!", self)
         self.button3.clicked.connect(self.button3Clicked)
         buttonLayout.addWidget(self.button3)
 
-        mainLayout.addLayout(buttonLayout)
+        buttonWidget = QWidget()
+        buttonWidget.setLayout(buttonLayout)
+        mainLayout.addWidget(buttonWidget)
         self.setLayout(mainLayout)
+
+    def get_position(self):
+        for row in range(3):
+            for col in range(3):
+                if self.radioButtons[row][col].isChecked():
+                    return row, col
+        self.text.append("Got an error on selecting watermark position!")
+        return None
 
     def button0Clicked(self):
         fname = QFileDialog.getExistingDirectory(self, "Select Folder", "")
@@ -77,7 +116,9 @@ class MainWindow(QWidget, Ui_MainWindow):
         self.text.append(f"Black watermark selected: {fname[0]}")
 
     def button3Clicked(self):
-        self.AutoWatermark(self.images, self.ratio, self.white, self.black)
+        self.AutoWatermark(
+            self.images, self.watermarkSizeInput.value(), self.white, self.black
+        )
         self.text.append("Applying watermarks complete!")
 
     def kor_imread(self, path):
@@ -89,6 +130,12 @@ class MainWindow(QWidget, Ui_MainWindow):
         return cv2.imdecode(img_array, cv2.IMREAD_UNCHANGED)
 
     def AutoWatermark(self, image, ratio, white, black):
+        position = self.get_position()
+        if position is None:
+            return
+
+        row, col = position
+
         for item in image:
             item = item.replace("\\", "/").replace("\\\\", "/")
             with open(item, "rb") as f:
@@ -107,7 +154,7 @@ class MainWindow(QWidget, Ui_MainWindow):
                 int(w_img - (w_img * ratio)) : w_img,
             ]
 
-            if np.mean(dst) / 255 > 0.5:
+            if np.mean(dst) / 255 > 0.4:
                 logo = self.kor_pngread(black)
             else:
                 logo = self.kor_pngread(white)
@@ -127,12 +174,28 @@ class MainWindow(QWidget, Ui_MainWindow):
             alpha = np.expand_dims(alpha, axis=2)
             alpha = np.repeat(alpha, 3, axis=2)
 
-            destination = img[h_img - h_logo : h_img, w_img - w_logo : w_img]
+            center_y = (
+                h_img - h_logo / 2
+                if row == 2
+                else (h_logo / 2 if row == 0 else h_img / 2)
+            )
+            center_x = (
+                w_img - w_logo / 2
+                if col == 2
+                else (w_logo / 2 if col == 0 else w_img / 2)
+            )
+
+            top_y = int(center_y - h_logo / 2)
+            bottom_y = top_y + h_logo
+            left_x = int(center_x - w_logo / 2)
+            right_x = left_x + w_logo
+
+            destination = img[top_y:bottom_y, left_x:right_x]
             result = cv2.multiply(destination.astype(float), (1 - (alpha / 255)))
             result += cv2.multiply(rgb.astype(float), (alpha / 255))
             result = result.astype(np.uint8)
 
-            img[h_img - h_logo : h_img, w_img - w_logo : w_img] = result
+            img[top_y:bottom_y, left_x:right_x] = result
 
             result_image = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
@@ -146,7 +209,7 @@ class MainWindow(QWidget, Ui_MainWindow):
             # if result:
             #     with open(item, mode="w+b") as f:
             #         encoded_img.tofile(f)
-            # else:
+            # elif:
             #     self.text.append(f"Failed to write image: {item}")
 
 
@@ -154,3 +217,5 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     mw = MainWindow()
     sys.exit(app.exec_())
+
+# >> pyinstaller -w -F --add-data="AutoWatermark.ui;./" AutoWatermark.py
